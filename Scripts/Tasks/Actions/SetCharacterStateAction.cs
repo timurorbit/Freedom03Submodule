829775814d85ac875b21ae16eaa1f493a52e51ec
Timur <t.shamilov.hollow@gmail.com>
2026-01-02 16:50:28 +0100

    Interactable State + SetCharacterAction

---
+++ Scripts/Tasks/Actions/SetCharacterStateAction.cs
@@ -0,0 +1,28 @@
+using _Game.Scripts.Behaviours;
+using Opsive.BehaviorDesigner.Runtime.Tasks;
+using Opsive.BehaviorDesigner.Runtime.Tasks.Actions;
+using Opsive.GraphDesigner.Runtime.Variables;
+using UnityEngine;
+
+public class SetCharacterStateAction : Action
+{
+    private CharacterBehaviour characterBehaviour;
+    
+    [SerializeField]
+    protected SharedVariable<CharacterState> characterState;
+
+    public override void OnAwake()
+    {
+        base.OnAwake();
+        characterBehaviour = gameObject.GetComponent<CharacterBehaviour>();
+    }
+
+    public override TaskStatus OnUpdate()
+    {
+        if (characterBehaviour == null)
+            return TaskStatus.Failure;
+
+        characterBehaviour.SetIdle();
+        return TaskStatus.Success;
+    }
+}
